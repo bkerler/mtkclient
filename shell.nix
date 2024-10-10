d48231c99eb9ee449b2a1e488de0515a08fd1d56
Bignaux Ronan <ronan@aimao.org>
2024-10-10 17:42:59 +0200

    nixfmt shell.nix

--- shell.nix
+++ shell.nix
@@ -1,30 +1,30 @@
-with import <nixpkgs> {};
+with import <nixpkgs> { };
 
 mkShell {
 
   name = "mtkclient";
-  
-  buildInputs = with python3Packages; [
-		capstone
-		colorama
-		flake8
-		fusepy
-		keystone
-		keystone-engine
-		mock
-		pycryptodome
-		pycryptodomex
-		pyserial
-		pyside6
-		pyusb
-		setuptools
-		shiboken6
-		unicorn
-		wheel
 
-  # workaround : use qttools in place of pyside6 script
-  # since they are missing https://github.com/NixOS/nixpkgs/issues/277849
+  buildInputs = with python3Packages; [
+    capstone
+    colorama
+    flake8
+    fusepy
+    keystone
+    keystone-engine
+    mock
+    pycryptodome
+    pycryptodomex
+    pyserial
+    pyside6
+    pyusb
+    setuptools
+    shiboken6
+    unicorn
+    wheel
+
+    # workaround : use qttools in place of pyside6 script
+    # since they are missing https://github.com/NixOS/nixpkgs/issues/277849
     qt5.qttools
-	];
+  ];
 
 }
