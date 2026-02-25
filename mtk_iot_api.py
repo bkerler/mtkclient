af6f7105f8db3ed649a458892a6c81da5dc27aa1
Bjoern Kerler <info@revskills.de>
2026-02-25 20:55:53 +0100

    Fix payload patching for 64bit, add minor changes

--- mtk_iot_api.py
+++ mtk_iot_api.py
@@ -30,15 +30,29 @@ def connect(mtk, directory=".", loglevel=logging.INFO):
 
 
 def main():
+    if len(sys.argv)<2:
+        print("Usage: python mtk_iot_api.py /dev/ttyUSB0")
+        sys.exit(1)
+    port = sys.argv[1]
     bytestoread=0x400000
     pos=0
-    mtk = init(preloader=None, loader=None,serialport="/dev/ttyUSB0")
+    mtk = init(preloader=None, loader=None,serialport=port)
     mtk.config.iot = True
+    offs={}
     with open("dump.bin","wb") as wf:
         while bytestoread>0:
             inited = mtk.preloader.init(directory="")
             if inited:
                 data=mtk.preloader.dump_internal_flash(offset=pos,length=bytestoread,step=0x1000,filename="")
+                if len(data)==0:
+                    if not pos in offs:
+                        offs[pos]=0
+                    offs[pos]+=1
+                    if offs[pos]==2:
+                        print(f"Error reading at offset {hex(pos)} ... skipping")
+                        wf.write(b"\xFF"*0x1000)
+                        bytestoread-=0x1000
+                        pos+=0x1000
                 if len(data)>0:
                     pos+=len(data)
                     bytestoread-=len(data)
--- mtkclient/Library/DA/xmlflash/xml_lib.py
+++ mtkclient/Library/DA/xmlflash/xml_lib.py
@@ -615,7 +615,8 @@ class DAXML(metaclass=LogBase):
             self.heapbait = Heapbait(self.mtk, self.loglevel)
 
             if not self.mtk.daloader.patch and not self.mtk.config.stock:
-                if self.mtk.config.target_config["sbc"] and self.carbonara.check_for_carbonara_patched(self.daconfig.da1):
+                if self.mtk.config.target_config["sbc"] and not self.carbonara.check_for_carbonara_patched(
+                        self.daconfig.da1):
                     loaded = self.carbonara.patchda1_and_upload_da2()
                     if loaded:
                         self.mtk.daloader.patch = True
